5511f40ac2d630bb0f570d074e14426c16bee7fa
ariefzuhri <ariefzuhri72@gmail.com>
2021-06-23 09:34:55 +0700

    Fix null property crash when checking for favorite updates

--- app/src/main/java/com/ariefzuhri/movee/utils/AppUtils.java
+++ app/src/main/java/com/ariefzuhri/movee/utils/AppUtils.java
@@ -14,13 +14,18 @@ import static com.ariefzuhri.movee.utils.Constants.BASE_URL_TMDB_IMAGE;
 public class AppUtils {
 
     public static boolean equalsFavoriteObjects(FavoriteEntity o1, FavoriteEntity o2){
-        return o1.getId().equals(o2.getId()) &&
-                o1.getType().equals(o2.getType()) &&
-                o1.getTitle().equals(o2.getTitle()) &&
-                o1.getPoster().equals(o2.getPoster()) &&
-                o1.getScoreAverage() == o2.getScoreAverage() &&
-                o1.getStartDate().equals(o2.getStartDate()) &&
-                o1.getGenres().size() == o2.getGenres().size();
+        try {
+            return o1.getId().equals(o2.getId()) &&
+                    o1.getType().equals(o2.getType()) &&
+                    o1.getTitle().equals(o2.getTitle()) &&
+                    o1.getPoster().equals(o2.getPoster()) &&
+                    o1.getScoreAverage() == o2.getScoreAverage() &&
+                    o1.getStartDate().equals(o2.getStartDate()) &&
+                    o1.getGenres().size() == o2.getGenres().size();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public static void loadImage(Context context, String size, Object source, ImageView imageView) {
