2f6c6d5bb82624a2b7125c2e4bd7c3dc7fdf604c
ariefzuhri <ariefzuhri72@gmail.com>
2021-06-23 09:26:41 +0700

    Fix crash when the release date is null

--- app/src/main/java/com/ariefzuhri/movee/utils/DateUtils.java
+++ app/src/main/java/com/ariefzuhri/movee/utils/DateUtils.java
@@ -19,29 +19,34 @@ public class DateUtils {
     }
 
     public static String getDateWithoutYear(String date){
+        if (date == null) return "N/A";
         Date formattedDate = stringToDate(date);
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(formattedDate);
-        return String.format("%s %s",
-                new DateFormatSymbols().getMonths()[calendar.get(Calendar.MONTH)],
-                calendar.get(Calendar.DAY_OF_MONTH)
-        );
+        if (formattedDate != null) {
+            calendar.setTime(formattedDate);
+            return String.format("%s %s",
+                    new DateFormatSymbols().getMonths()[calendar.get(Calendar.MONTH)],
+                    calendar.get(Calendar.DAY_OF_MONTH));
+        } else return "N/A";
     }
 
     public static String getYearOfDate(String date){
+        if (date == null) return "N/A";
         Date formattedDate = stringToDate(date);
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(formattedDate);
-        return String.valueOf(calendar.get(Calendar.YEAR));
+        if (formattedDate != null) {
+            calendar.setTime(formattedDate);
+            return String.valueOf(calendar.get(Calendar.YEAR));
+        } else return "N/A";
     }
 
-    public static Date stringToDate(String date) {
+    private static Date stringToDate(String date) {
         try {
             DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
             return dateFormat.parse(date);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        return new Date();
+        return null;
     }
 }
